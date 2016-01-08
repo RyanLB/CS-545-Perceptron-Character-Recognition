47a6d4596c0ad5085a30e0ee8d4747e799b18e00
Ryan Bernstein <lbryanlb@comcast.net>
2016-01-07 16:28:46 -0800

    Initial Commit

---
+++ Perceptron Character Recognition/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Perceptron Character Recognition
+//
+//  Created by Ryan Bernstein on 1/7/16.
+//  Copyright © 2016 Ryan Bernstein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
