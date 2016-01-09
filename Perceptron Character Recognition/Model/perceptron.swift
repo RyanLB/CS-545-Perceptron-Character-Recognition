c25b32e34a644efbee573faec205a3f6ce394b7c
Ryan Bernstein <lbryanlb@comcast.net>
2016-01-09 13:58:52 -0800

    Add functionality to Perceptron.
    It can now:
    * Initialize itself with a variable number of weights
    * Run itself on input data
    * Take a dot product
    * Get accuracy for a series of entries (untested)

--- Perceptron Character Recognition/Model/perceptron.swift
+++ Perceptron Character Recognition/Model/perceptron.swift
@@ -8,26 +8,87 @@
 
 import Foundation
 
+enum PerceptronError : ErrorType {
+    case InvalidLength(length: Int)
+    case NonMatchingLengths(l1: Int, l2: Int)
+}
+
 class Perceptron {
-    let bias: Double
-    let weights: Array<Double>
+    let weightCount: Int
+    
+    var bias: Double
+    private var _weights: [Double]
+    
+    var weights: [Double] {
+        get {
+            return _weights
+        }
+        set {
+            if newValue.count == weightCount {
+                _weights = newValue
+            }
+        }
+    }
     
     // Initialize with random weights
-    init() {
+    init(withWeightCount: Int) {
+        weightCount = withWeightCount
         bias = Perceptron.smallRandom()
         
         var weightArray:[Double] = []
-        for _ in [0..<16] {
+        for _ in [0..<weightCount] {
             weightArray.append(Perceptron.smallRandom())
         }
         
-        weights = weightArray
+        _weights = weightArray
     }
     
-    init(withBias: Double, andWeights: Array<Double>) {
-        bias = withBias
-        weights = andWeights
+    convenience init() {
+        self.init(withWeightCount: 16)
     }
+    
+    init(withBias: Double, andWeights: [Double]) {
+        bias = withBias
+        weightCount = andWeights.count
+        _weights = andWeights
+    }
+    
+    /**
+     Runs this Perceptron on the given array of inputs.
+     
+     - Parameter inputArray: The input vector on which to test this Perceptron.
+     
+     - Throws: `NonMatchingLengths` if `inputArray.count != weightCount`.
+     
+     - Returns: `true` if the Perceptron "fires" and false otherwise.
+
+     */
+    
+    func run(inputArray: [Double]) throws -> Bool {
+        guard inputArray.count == weightCount else {
+            throw PerceptronError.NonMatchingLengths(l1: weightCount, l2: inputArray.count)
+        }
+        
+        return try (bias + dotProduct(inputArray, secondArr: weights)) >= 0
+    }
+    
+    func testAccuracy(trainingData: [([Double], Bool)]) -> Int {
+        return trainingData.reduce(0, combine: {
+            let result = try? run($1.0)
+            if result == $1.1 {
+                return $0 + 1
+            }
+            
+            return $0
+        })
+    }
+    
+    func train(trainingData: [([Double], Bool)], learningRate: Double) {
+        
+    }
+    
+
+    ///Generates a small random number between -1 and 1.
     
     private class func smallRandom() -> Double {
         let randVal = drand48()
@@ -36,5 +97,19 @@ class Perceptron {
         }
         
         return randVal
+    }
+    
+    /**
+     Calculates the dot product of two Double arrays.
+     
+     - Throws: `NonMatchingLengths` if the two input arrays have differeing lenghts.
+     */
+    
+    private func dotProduct(firstArr: [Double], secondArr: [Double]) throws -> Double {
+        guard firstArr.count == secondArr.count else {
+            throw PerceptronError.NonMatchingLengths(l1: firstArr.count, l2: secondArr.count)
+        }
+        
+        return zip(firstArr, secondArr).reduce(0.0, combine: { $0 + ($1.0 * $1.1) })
     }
 }
