b218c467c1d6ab3180f0b3df4c4bd7995060c8a5
Ryan Bernstein <lbryanlb@comcast.net>
2016-01-08 16:47:44 -0800

    Create NSFileHandle extension that can get ASCII characters and lines

---
+++ Perceptron Character Recognition/NSFileHandleExtension.swift
@@ -0,0 +1,33 @@
+//
+//  NSFileHandleExtension.swift
+//  perceptron_chars
+//
+//  Created by Ryan Bernstein on 1/8/16.
+//  Copyright © 2016 Ryan Bernstein. All rights reserved.
+//
+
+import Foundation
+
+extension NSFileHandle {
+    func getASCIIChar() -> Character? {
+        let byteData = readDataOfLength(1)
+        
+        let dataString = String(data: byteData, encoding: NSASCIIStringEncoding)
+        
+        return dataString?[(dataString?.startIndex)!]
+    }
+    
+    func getASCIILine() -> String? {
+        var chars = ""
+        
+        while let c = getASCIIChar() {
+            if c == "\n" {
+                return chars
+            }
+            
+            chars.append(c)
+        }
+        
+        return chars == "" ? nil : chars
+    }
+}
--- Perceptron Character Recognition/main.swift
+++ Perceptron Character Recognition/main.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 
-print("Hello, World!")
+print(Process.arguments[0])
+
+if Process.arguments.count < 2 {
+    print("usage: perceptron_chars [training data path] [test data path]")
+    exit(EXIT_FAILURE)
+}
+
+let path = Process.arguments[1]
+let trainingHandle = NSFileHandle(forReadingAtPath: Process.arguments[1])
+
+if trainingHandle == nil {
+    print("Invalid filepath for training data.")
+    exit(EXIT_FAILURE)
+}
+
+print(trainingHandle!.getASCIILine()!)
+
+/*while let line = handle?.getLine() {
+    NSLog("LINE: %s\n", line)
+}
+*/
 
