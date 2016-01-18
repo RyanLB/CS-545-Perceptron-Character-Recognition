98a96e10fb62abdea484b4f8f9a57e7a02f433cc
Ryan Bernstein <lbryanlb@comcast.net>
2016-01-17 18:54:14 -0800

    Update iteration in network training. Remove unused (inefficient) Perceptron location functions.

--- Perceptron Character Recognition/Model/LetterRecognitionNetwork.swift
+++ Perceptron Character Recognition/Model/LetterRecognitionNetwork.swift
@@ -32,9 +32,8 @@ class LetterRecognitionNetwork {
     }
     
     func trainPerceptrons(trainingData: [Character: [Letter]], learningRate: Double) {
-        for c1 in perceptronNetwork.keys {
-            let subNetwork = perceptronNetwork[c1]!
-            for c2 in subNetwork.keys {
+        for (c1, subNetwork) in perceptronNetwork {
+            for (c2, p) in subNetwork {
                 
                 var relevantExamples = [([Double], Bool)]()
                 if let c1Examples = trainingData[c1] {
@@ -44,7 +43,7 @@ class LetterRecognitionNetwork {
                     relevantExamples += c2Examples.map({ (l: Letter) -> ([Double], Bool) in (l.attributeVector, true) } )
                 }
                 
-                _ = try? perceptronFor(c1, c2: c2).train(relevantExamples, learningRate: learningRate)
+                p.train(relevantExamples, learningRate: learningRate)
             }
         }
     }
@@ -72,95 +71,14 @@ class LetterRecognitionNetwork {
         return Character(UnicodeScalar(winners[winIndex] + 65))
     }
     
+    /**
+     Helper function that converts a character to an array index between 0 and 25.
+     
+     Assumes that this is a capital letter between "A" and "Z", ASCII encoded.
+     */
     private func charToAZIndex(c: Character) -> Int {
         let s = String(c).unicodeScalars
         return Int(s[s.startIndex].value) - 65
-    }
-    
-    private func perceptronFor(c1: Character, c2: Character) throws -> Perceptron {
-        guard c1 != c2 else {
-            throw NeuralNetworkError.SelfComparison(c: c1)
-        }
-        
-        let s1 = String(c1).unicodeScalars
-        let ord1 = s1[s1.startIndex].value
-        
-        let s2 = String(c2).unicodeScalars
-        let ord2 = s2[s2.startIndex].value
-        
-        guard (65...90).contains(ord1) else {
-            throw NeuralNetworkError.CharacterOutOfRange(c: c1)
-        }
-        
-        guard (65...90).contains(ord2) else {
-            throw NeuralNetworkError.CharacterOutOfRange(c: c2)
-        }
-        
-        if ord2 < ord1 {
-            return try perceptronFor(c2, c2: c1)
-        }
-        
-        if let p = perceptronNetwork[c1]?[c2] {
-            return p
-        }
-        
-        throw NeuralNetworkError.NotFoundError(c1: c1, c2: c2)
-    }
-    
-    private func perceptronsContaining(c: Character) throws -> [Perceptron] {
-        let s = String(c).unicodeScalars
-        let ord = s[s.startIndex].value
-        
-        guard (65...90).contains(ord) else {
-            throw NeuralNetworkError.CharacterOutOfRange(c: c)
-        }
-        
-        let precedents = try (65..<ord).map{ try perceptronFor(Character(UnicodeScalar($0)), c2: c) }
-        let descendents = try ((ord + 1)...90).map{ try perceptronFor(c, c2: Character(UnicodeScalar($0))) }
-        
-        return precedents + descendents
-    }
-    
-    private func perceptronsWithFirstLetter(c: Character) -> [Perceptron] {
-        let s = String(c).unicodeScalars
-        let ord = s[s.startIndex].value
-        
-        guard (65...90).contains(ord) else {
-            return []
-        }
-        
-        if let subNetwork = perceptronNetwork[c] {
-            var results = [Perceptron]()
-            for c2 in subNetwork.keys {
-                if let p = subNetwork[c2] {
-                    results.append(p)
-                }
-            }
-            return results
-        }
-        else {
-            return []
-        }
-    }
-    
-    private func perceptronsWithSecondLetter(c: Character) -> [Perceptron] {
-        let s = String(c).unicodeScalars
-        let ord = s[s.startIndex].value
-        
-        guard (65...90).contains(ord) else {
-            return []
-        }
-        
-        let precedents = (65..<ord).map{ Character(UnicodeScalar($0)) }
-        var results = [Perceptron]()
-        
-        for c2 in precedents {
-            if let p = perceptronNetwork[c2]?[c] {
-                results.append(p)
-            }
-        }
-        
-        return results
     }
     
 }
